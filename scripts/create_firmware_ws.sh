01f38a09b2ebb6438793ff451ba19b5166315a14
adityakri <105853079+adityakri@users.noreply.github.com>
2022-05-20 18:48:11 +0530

    Update create_firmware_ws.sh

--- scripts/create_firmware_ws.sh
+++ scripts/create_firmware_ws.sh
@@ -28,7 +28,7 @@ print_available_platforms () {
   done
   popd >/dev/null
 }
-
+echo "create_firmware_ws.sh line 31"
 # Retrieving RTOS and Platform
 
 if [ $# -ge 1 ]; then
@@ -38,7 +38,7 @@ else
     print_available_platforms
     exit 1
 fi
-
+echo "create_firmware_ws.sh line 31"
 if [ $# -ge 2 ]; then
     PLATFORM=$2
 else
@@ -50,7 +50,7 @@ if [ -d $FW_TARGETDIR ]; then
     echo "Firmware already created. Please delete $FW_TARGETDIR folder if you want a fresh installation."
     exit 1
 fi
-
+echo "create_firmware_ws.sh line 53"
 # Checking folders
 if [ -d $PREFIX/config/$RTOS/$PLATFORM ] || [ -d "$PREFIX/config/$RTOS/generic" ]; then
     echo "Creating firmware for $RTOS platform $PLATFORM"
@@ -60,13 +60,13 @@ else
     print_available_platforms
     exit 1
 fi
-
+echo "create_firmware_ws.sh line 63"
 mkdir $FW_TARGETDIR
 touch $FW_TARGETDIR/COLCON_IGNORE
 
 echo $RTOS > $FW_TARGETDIR/PLATFORM
 echo $PLATFORM >> $FW_TARGETDIR/PLATFORM
-
+echo "create_firmware_ws.sh line 69"
 # Setting common enviroment
 
 if [ -z ${EXTERNAL_SKIP+x} ]; then
@@ -74,27 +74,28 @@ if [ -z ${EXTERNAL_SKIP+x} ]; then
 fi
 
 SKIP="microxrcedds_agent microxrcedds_client microcdr rosidl_typesupport_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_opensplice_cpp rosidl_typesupport_opensplice_c rmw_opensplice_cpp ros-${ROS_DISTRO}-cyclonedds  ros-${ROS_DISTRO}-rti-connext-dds-cmake-module ros-${ROS_DISTRO}-rmw-connextdds-common ros-${ROS_DISTRO}-rmw-connextdds ros-${ROS_DISTRO}-rmw-cyclonedds-cpp google_benchmark_vendor performance_test_fixture ros-${ROS_DISTRO}-mimick-vendor rmw_cyclonedds_cpp rmw_connext_cpp rti-connext-dds-5.3.1 rmw_connextdds $EXTERNAL_SKIP"
-
+echo "create_firmware_ws.sh line 77"
 # Check generic build
 if [ $PLATFORM != "generic" ] && [ -d "$PREFIX/config/$RTOS/generic" ]; then
     TARGET_FOLDER=generic
 else
     TARGET_FOLDER=$PLATFORM
 fi
-
+echo "create_firmware_ws.sh line 85"
 pushd $FW_TARGETDIR >/dev/null
     # Creating dev directory
     mkdir $DEV_WS_DIR
-
+echo "create_firmware_ws.sh line 88"
     if [ $RTOS != "host" ]; then
         ros2 run micro_ros_setup create_ws.sh $DEV_WS_DIR $PREFIX/config/$RTOS/dev_ros2_packages.txt \
             $PREFIX/config/$RTOS/dev_uros_packages.repos
         rosdep install -y --from-paths $DEV_WS_DIR -i $DEV_WS_DIR --rosdistro $ROS_DISTRO --skip-keys="$SKIP"
-
+echo "create_firmware_ws.sh line 93"
          # Creating mcu directory
         mkdir mcu_ws
         ros2 run micro_ros_setup create_ws.sh mcu_ws $PREFIX/config/client_ros2_packages.txt $PREFIX/config/$RTOS/$TARGET_FOLDER/client_uros_packages.repos
         cp $PREFIX/config/$RTOS/$TARGET_FOLDER/client-colcon.meta mcu_ws/colcon.meta
+echo "create_firmware_ws.sh line 98"        
     fi
 popd >/dev/null
 
@@ -108,10 +109,10 @@ if [ $RTOS != "host" ]; then
         . install/setup.bash
     popd > /dev/null
 fi
-
+echo "create_firmware_ws.sh line 112"
 # Install dependecies for specific platform
 rosdep install -y --from-paths $PREFIX/config/$RTOS/$TARGET_FOLDER -i $PREFIX/config/$RTOS/$TARGET_FOLDER --rosdistro $ROS_DISTRO --skip-keys="$SKIP"
-
+echo "create_firmware_ws.sh line 115"
 # Creating specific firmware folder
 . $PREFIX/config/$RTOS/$TARGET_FOLDER/create.sh
 
