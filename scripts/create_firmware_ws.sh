fb98d012230716fd521ac4072eb2414fddfedfa8
adityakri <105853079+adityakri@users.noreply.github.com>
2022-05-20 18:09:09 +0530

    Update create_firmware_ws.sh

--- scripts/create_firmware_ws.sh
+++ scripts/create_firmware_ws.sh
@@ -95,6 +95,7 @@ pushd $FW_TARGETDIR >/dev/null
         mkdir mcu_ws
         ros2 run micro_ros_setup create_ws.sh mcu_ws $PREFIX/config/client_ros2_packages.txt $PREFIX/config/$RTOS/$TARGET_FOLDER/client_uros_packages.repos
         cp $PREFIX/config/$RTOS/$TARGET_FOLDER/client-colcon.meta mcu_ws/colcon.meta || :
+        echo "mcu done!"
     fi
 popd >/dev/null
 
