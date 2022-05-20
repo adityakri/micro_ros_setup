e6d60b9b42ced93d0d5477d2a36bdaccb541a4b8
adityakri <105853079+adityakri@users.noreply.github.com>
2022-05-20 18:44:27 +0530

    Update create.sh

--- config/freertos/discovery_l475_iot1/create.sh
+++ config/freertos/discovery_l475_iot1/create.sh
@@ -2,13 +2,13 @@ pushd $FW_TARGETDIR >/dev/null
     # Install toolchain
     mkdir toolchain
 
-
+   
     # Install toolchain
     echo "Downloading ARM compiler, this may take a while"
     curl -fsSLO https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2
     tar --strip-components=1 -xvjf gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2 -C toolchain  > /dev/null
     rm gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2
-
+    echo "import board repos"
     # Import repos
     vcs import --input $PREFIX/config/$RTOS/$PLATFORM/board.repos
 
@@ -23,4 +23,5 @@ pushd $FW_TARGETDIR >/dev/null
     touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
 
     rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro $ROS_DISTRO --skip-keys="$SKIP"
+    echo "creat.sh line 26"
 popd >/dev/null
