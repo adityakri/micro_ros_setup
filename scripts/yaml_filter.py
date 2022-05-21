77ddb00ee46484ff8b1822b7cb0530a420c5cc03
adityakri <105853079+adityakri@users.noreply.github.com>
2022-05-21 13:57:10 +0530

    Update yaml_filter.py

--- scripts/yaml_filter.py
+++ scripts/yaml_filter.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     repos_keep = yaml.safe_load(open(sys.argv[1]))['keep'].split()
 
     target = {'repositories': {}}
-    echo "yaml_filter.py line 18"
+   
     if repos_info:
         for key in repos_info["repositories"]:
             if key in repos_keep:
                 target['repositories'][key] = repos_info["repositories"][key]
-    echo "yaml_filter.py line 19"
+   
     print(yaml.dump(target))
-    echo "yaml_filter.py line last"
+   
